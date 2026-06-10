45d0941ef9fcdaac95a95da32022e105340add9e
sheikaabu11 <sheikaabuthaircyber11@gmail.com>
2026-06-10 23:46:58 +0530

    ODD or Even JAVA

--- Day2.java
+++ Day2.java
@@ -10,14 +10,23 @@ public class Day2{
         System.out.print("Enter c :");
         int c = sc.nextInt();
 
-        if(a >= b && a >= c){
-            System.out.println(a +" is largest in 3 number");
-        }
-        else if(b >= a && b >= c) {
-            System.out.println(b +" is largest in 3 number");
+// Check if 3 largest Number
+        // if(a >= b && a >= c){
+        //     System.out.println(a +" is largest in 3 number");
+        // }
+        // else if(b >= a && b >= c) {
+        //     System.out.println(b +" is largest in 3 number");
+        // }
+        // else{
+        //     System.out.println(c +" is largest in 3 number");
+        // }
+        
+// Check its ODD OR EVEN
+        if(a % 2 != 0){
+            System.out.println( a + " is  ODD Number");
         }
         else{
-            System.out.println(c +" is largest in 3 number");
+            System.out.println( a + " is  EVEN Number");
         }
     }
 }
