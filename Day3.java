2474498cdaa91a13d4d6b03a7464e137932f559a
sheikaabu11 <sheikaabuthaircyber11@gmail.com>
2026-06-11 19:09:17 +0530

    Factorial

--- Day3.java
+++ Day3.java
@@ -4,11 +4,23 @@ public class Day3 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
+    // Check Sum
     // int sum = n * (n + 1)/2;
     // System.out.println( "Sum of First " + n + " number = " +sum);
-    for(int i= 1; i <= 10 ; i++){
-        System.out.println(n+ " X " + i + "  = " + (n*i));
+
+    //  Multiple Table
+    // for(int i= 1; i <= 10 ; i++){
+    //     System.out.println(n+ " X " + i + "  = " + (n*i));
+    // }
+
+    // Factorial
+
+    long Factorial = 1;
+    for (int i = 1; i <= n; i++){
+        Factorial = Factorial * i;
     }
+    System.out.println("factorial of " + n + " = " + Factorial);
 }
+
     
 }
