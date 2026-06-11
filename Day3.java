13d0cb9a8106c5b3edb72b3ab6893542b77f6ce2
sheikaabu11 <sheikaabuthaircyber11@gmail.com>
2026-06-11 19:02:50 +0530

    Sum of N

---
+++ Day3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Day3 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int sum = n * (n + 1)/2;
+    System.out.println( "Sum of First " + n + " number = " +sum);
+
+
+}
+    
+}
