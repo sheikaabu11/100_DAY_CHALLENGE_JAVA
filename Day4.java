1a3b27e1093be022832ef1033617f7ac25a6f5ea
sheikaabu11 <sheikaabuthaircyber11@gmail.com>
2026-06-12 22:32:47 +0530

    Prime Number

--- Day4.java
+++ Day4.java
@@ -2,14 +2,30 @@ import java.util.Scanner;
 
 public class Day4 {
     public static void main(String[] args) {
+        // Find the Number of Digits 
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int count = 0;
+        // while(n != 0){
+        //     n = n/10;
+        //     count +=1;
+        // }
+        // System.out.println("The number of Digit is : "+count);
+
+        // Prime Number 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int count = 0;
-        while(n != 0){
-            n = n/10;
-            count +=1;
+        for(int i = 1 ;  i <= n; i++){
+            if(n % i == 0){
+                count++;
+            }
         }
-        System.out.println("The number of Digit is : "+count);
-
+        if (count == 2){
+            System.out.println("Prime Number");
+        }
+        else{
+            System.out.println("Not Prime Number ");
+        }
     }
 }
